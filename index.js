eccbe464611786e1497c87b6442fa0bdd4b35541
malasia-415 <55638544+malasia-415@users.noreply.github.com>
2020-01-12 09:07:21 -0800

    more images, format created js file aliment for css

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //Add Javascript//
+
+$(".jumper").on("click", function(event) {
+    
+    e.preventDefault();
+
+    $("body, html").animate({ 
+        scrollTop: $( $(this).attr('href') ).offset().top 
+    }, 360);
+    
+});
